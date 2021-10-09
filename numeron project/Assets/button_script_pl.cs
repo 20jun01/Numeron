69fd5d5ab24ca783226c418ff310c5a9d321aa32
20jun01 <10junjun20@gmail.com>
2021-10-09 22:46:52 +0900

    Onclickの追加

---
+++ numeron project/Assets/button_script_pl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button_script_pl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // when  button tapped, this function will be called
+    public void OnClick(){
+        // write code here.
+        Debug.Log("押されました...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
