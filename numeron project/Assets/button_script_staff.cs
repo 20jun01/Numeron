d34e9de21216696462dd012cdb20f4ec8cfd6dd0
20jun01 <10junjun20@gmail.com>
2021-10-11 18:31:05 +0900

    implement staff button
    if you want change image, look at the Image_staff and change  Source Image.

--- numeron project/Assets/button_script_staff.cs
+++ numeron project/Assets/button_script_staff.cs
@@ -10,6 +10,7 @@ using UnityEngine.AddressableAssets;
 public class button_script_staff : MonoBehaviour
 {
     private AudioSource sound_tap;
+    [SerializeField] GameObject Image;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,27 +21,13 @@ public class button_script_staff : MonoBehaviour
     public void OnClick(){
         // write code here.
         sound_tap.PlayOneShot(sound_tap.clip);
-        button_script_staff.change_scene();
+        // button_script_staff.change_scene();
+        Image.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    // delay function
-    static async void change_scene(){
-        await Task.Delay(350);
-        // Get GameObject and show with Addressables.
-        // If you want more information, click here.(https://light11.hatenadiary.com/entry/2019/12/26/225232)
-        Addressables
-            .LoadAssetAsync<GameObject>("Image_staff") // アドレスを文字列で指定
-            .Completed += op => {
-                // 結果を取得してインスタンス化
-                // 本来はエラーハンドリングなど必要
-                Instantiate(op.Result);
-            };
-        // SceneManager.LoadScene("Select_num");
     }
 }
