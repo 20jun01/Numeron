cf9bf3a18b6c8a339f7afd123c268a48f421292e
20jun01 <10junjun20@gmail.com>
2021-10-17 22:19:35 +0900

    add result

--- numeron project/Assets/image_rurle_last.cs
+++ numeron project/Assets/image_rurle_last.cs
@@ -2,18 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class script_staff : MonoBehaviour
+public class image_rurle_last : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    public void OnClick(){
-        this.gameObject.SetActive(false);
-    }
-
 
     // Update is called once per frame
     void Update()
---
+++ numeron project/Assets/scripts/image_rule.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class image_rule : MonoBehaviour
+{
+    [SerializeField] GameObject Image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnClick(){
+        this.gameObject.SetActive(false);
+        Image.SetActive(true);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
